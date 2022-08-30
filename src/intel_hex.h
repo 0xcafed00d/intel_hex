0fbfdd8b72e7590c51b014b23997b7494e543e8b
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2022-08-30 02:35:32 +0100

    adding reader

--- src/intel_hex.h
+++ src/intel_hex.h
@@ -1,62 +1,114 @@
 #ifndef BC45DBCC_061E_4977_B2B2_0C1D57025446
 #define BC45DBCC_061E_4977_B2B2_0C1D57025446
 
-#include <string_view>
-#include <cstdint>
 #include <algorithm>
+#include <cctype>
+#include <cstdint>
+#include <exception>
+#include <type_traits>
 
-// fileWriter -> void write(std::string_view s) -- throws on error
+// fileWriter -> void write(const char* str) -- throws on error
+// fileReader -> char read() -- throws on error - returns 0 on eof
 // memReader -> std::uint8_t peek(std::unit16_t address)
+// memWriter -> void poke(std::unit16_t address, std::uint8_t val)
 
-namespace intel_hex{
+namespace intel_hex {
+	namespace helpers {
 
-    template <typename memReader>
-    std::uint8_t calc_checksum(memReader& r, std::uint16_t address, size_t len){
-        std::uint16_t tot = 0;
-        
-        while (len--){
-            tot += r(address);
-            address++;
-        }
+		template <typename fileWriter>
+		void write_byte(fileWriter& w, std::uint8_t val) {
+			char buffer[3];
+			sprintf(buffer, "%02x", val);
+			w(buffer);
+		}
 
-        return std::uint8_t(-tot);
-    }
+		template <typename fileWriter>
+		void write_word(fileWriter& w, std::uint16_t val) {
+			write_byte(w, val >> 8);
+			write_byte(w, val);
+		}
+	}  // namespace helpers
 
-    template <typename fileWriter>
-    void write_byte (fileWriter& w, std::uint8_t b){
-        char buffer[3];
-        sprintf(buffer, "%02x", b);
-        w(buffer);
-    }
+	// ---------------------------------------------------------------------------------------
+	// Writing intel hex files ....
+	// ---------------------------------------------------------------------------------------
 
-    template <typename fileWriter>
-    void write_word (fileWriter& w, std::uint16_t w){
-        char buffer[5];
-        sprintf(buffer, "%04x", w);
-        w(buffer);
-    }
+	template <typename fileWriter, typename memReader>
+	void write_data(fileWriter w, memReader& r, std::uint16_t address, size_t len) {
+		using namespace helpers;
 
-    template <typename fileWriter, typename memReader>
-    void write_data (fileWriter& w, memReader& r, std::uint16_t address, size_t len){        
-        while (len){
-            size_t line_len = std::min<size_t, size_t>(16, len);
-            len -= line_len;
+		static_assert(std::is_invocable_v<fileWriter, const char*>,
+		              "fileWriter must be invocable as: "
+		              "void fileWriter(const char* str)");
 
-            std::uint8_t check = calc_checksum(r, address, len); 
-            
-            w(":");
-            write_byte(w, line_len);
-            write_word(w, address);
-            w("00");
-            write_byte(w, check);
-            w("\n");
-        }
-    }
+		static_assert(std::is_invocable_r_v<std::uint8_t, memReader, std::uint16_t>,
+		              "memReader must be invocable as: "
+		              "std::uint8_t memReader(std::uint16_t adress)");
 
-    template <typename fileWriter>
-    void write_end (fileWriter& w){
-        w(":00000001FF");
-    }
-}
+		while (len) {
+			size_t line_len = std::min(size_t(16), len);
+			len -= line_len;
+
+			std::uint8_t check = line_len + (address & 0xff) + ((address >> 8) & 0xff);
+
+			w(":");
+			write_byte(w, line_len);
+			write_word(w, address);
+			w("00");
+			while (line_len--) {
+				auto val = r(address);
+				check += val;
+				write_byte(w, val);
+				address++;
+			}
+			write_byte(w, (~check) + 1);
+			w("\n");
+		}
+	}
+
+	template <typename fileWriter>
+	void write_end(fileWriter& w) {
+		w(":00000001FF");
+	}
+
+	template <typename fileWriter, typename memReader>
+	void write_all(fileWriter& w, memReader& r, std::uint16_t address, size_t len) {
+		write_data(w, r, address, len);
+		write_end(w);
+	}
+
+	// ---------------------------------------------------------------------------------------
+	// Reading intel hex files ....
+	// ---------------------------------------------------------------------------------------
+	namespace helpers {
+		template <typename fileReader>
+		bool read_byte(fileReader& r, std::uint8_t& val) {
+			char buffer[3];
+			buffer[0] = r();
+			buffer[1] = r();
+			buffer[2] = 0;
+
+			if (!std::isxdigit(buffer[0]) || !std::isxdigit(buffer[1])) {
+				// throw error
+			}
+
+			val = strtol(buffer, std::nullptr_t, 16);
+		}
+	}  // namespace helpers
+
+	template <typename fileReader, typename memWriter>
+	void read(fileReader& r, memWriter& w) {
+		using namespace helpers;
+
+		static_assert(std::is_invocable_r_v<char, fileReader>,
+		              "fileReader must be invocable as: "
+		              "char fileWriter()");
+
+		static_assert(std::is_invocable_v<memWriter, std::uint16_t, std::uint8_t>,
+		              "memReader must be invocable as: "
+		              "void memWriter(std::uint16_t address, std::uint8_t val)");
+	}
+
+}  // namespace intel_hex
 
 #endif /* BC45DBCC_061E_4977_B2B2_0C1D57025446 */
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <string_view>
 
 #include "intel_hex.h"
 
-void writer (std::string_view s){
-    std::cout << s << std::endl;
+void writer(std::string_view s) {
+	std::cout << s;
 }
 
-std::uint8_t peek(std::uint16_t addr){
-    return addr;
+std::uint8_t peek(std::uint16_t addr) {
+	return addr;
+}
+
+void poke(std::uint16_t addr, std::uint8_t val) {
+	std::cout << std::hex << addr << " <- " << val << std::endl;
 }
 
 int main() {
+	std::string output;
+	auto writer = [&output](const char* str) { output += str; };
+	intel_hex::write_all(writer, peek, 0x1000, 256);
 
-    intel_hex::write_data(writer, peek, 0x1000, 256);
-    intel_hex::write_end(writer);
-
+	auto reader = [&output, pos{0}]() mutable -> char { return output[pos++]; };
+	intel_hex::read(reader, poke);
 }
